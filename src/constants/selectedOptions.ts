bec8000dd7486e3f4d49f5f3d89e670442ef90d0
hyewonny2327 <wonny2327@gmail.com>
2025-05-01 20:56:56 +0900

    fix: select 타입 구체화

--- src/constants/selectedOptions.ts
+++ src/constants/selectedOptions.ts
@@ -1,6 +1,4 @@
-import { SelectOption } from "@custom-types/SelectOption";
-
-export const genderOptions: SelectOption[] = [
+export const genderOptions: SelectOption<GenderId>[] = [
   {
     id: "male",
     name: "남자",
@@ -16,23 +14,23 @@ export const genderOptions: SelectOption[] = [
     checked: false,
   },
 ];
-export const birthOptions: SelectOption[] = [
+export const birthOptions: SelectOption<BirthId>[] = [
   {
-    id: "0",
+    id: "solar",
     name: "양력",
     value: "solar-calendar",
     label: "양력",
     checked: true,
   },
   {
-    id: "1",
+    id: "lunar",
     name: "음력",
     value: "lunar-calendar",
     label: "음력",
     checked: false,
   },
   {
-    id: "2",
+    id: "lunar-leap",
     name: "음력(윤달)",
     value: "lunar-calendar-leap-month",
     label: "음력(윤달)",
--- src/types/SelectOption.ts
+++ src/types/SelectOption.ts
@@ -1,5 +1,8 @@
-export interface SelectOption {
-  id: string;
+type GenderId = "male" | "female";
+type BirthId = "solar" | "lunar" | "lunar-leap";
+
+interface SelectOption<ID extends string = string> {
+  id: ID;
   name: string;
   value: string;
   label: string;
