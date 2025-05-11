bdc6ec03c69e2061dc48a045527456813ba3949c
hyewonny2327 <wonny2327@gmail.com>
2025-05-11 23:32:28 +0900

    UI : 결과페이지 UI 포켓몬도감스타일로수정

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,17 +1,17 @@
 function UserProfile({ name }: { name: string | undefined }) {
   return (
-    <div className="w-[310px] h-[310px] rounded-[20px] shadow-2xl backdrop-blur-md bg-white/30 overflow-hidden flex flex-col items-center justify-between">
-      <div className="flex-1 flex items-center justify-center">
+    <div className="w-[310px] h-[310px] bg-white border-2 border-black rounded-[20px] shadow-[4px_4px_0px_black] overflow-hidden flex flex-col items-center justify-between">
+      <div className="flex-1 flex items-center justify-center p-4">
         <img
           src={`/pokeball_color.svg`}
           alt="pokeball"
-          className="w-[210px] h-auto"
+          className="w-[180px] h-auto"
         />
       </div>
-      <div
-        className={`w-full text-white h-[20%] flex items-center justify-center`}
-      >
-        <span className="text-black font-semibold text-lg">{name}</span>
+      <div className="w-full bg-yellow-100 border-t-2 border-black h-[20%] flex items-center justify-center">
+        <span className="text-black font-mono text-base tracking-wide">
+          {name} 님의 사주 리포트
+        </span>
       </div>
     </div>
   );
